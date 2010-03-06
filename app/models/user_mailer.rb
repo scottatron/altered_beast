a36bf8da2761bf46dd58f679f8aa41000e35bbcd
scottatron <scott@scottatron.com>
2010-03-06 16:19:37 +1300

    Set from & subject prefix in user mailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -14,8 +14,8 @@ class UserMailer < ActionMailer::Base
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "scott@drydairy.com"
+      @subject     = "[DryDairy] "
       @sent_on     = Time.now
       @body[:user] = user
     end
